f52a2c53bf12c614690cf3af058a8760c0575a7b
James S <i@nixeagle.org>
2010-07-07 23:38:29 +0000

    Remove remaining x3d stuff, its in cl-x3d now

--- opengl-import-experiments.lisp
+++ opengl-import-experiments.lisp
@@ -95,24 +95,7 @@ Vectors are: ~A" count vectors)
      for i from 0
      do (setf (get-array gl-array i components) vector)))
 
-(defun parse-sf-bool (string)
-  (declare (string string))
-  (check-type string (satisfies sf-bool-p))
-  (equalp "True" string))
 
-
-(defun parse-points (string)
-  "Parse a list of .x3d points into a list of vectors.
-
-Our result is something like (list #(1 2 3) #(3.4 3.4 4.5))"
-  (declare (string string))
-  (mapcar (lambda (vect)
-            (mapcar #'read-from-string
-                    (split-sequence:split-sequence
-                     #\Space vect :remove-empty-subseqs t)))
-          (remove " "
-                  (split-sequence:split-sequence #\, string :remove-empty-subseqs t)
-                  :test #'string=)))
 
 #+ () (defparameter *source* (cxml:parse-file "/home/james/blender25/.x3d" (cxml-dom:make-dom-builder)))
 
