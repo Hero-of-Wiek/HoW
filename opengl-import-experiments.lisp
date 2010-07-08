2fef65d91cd5265fd527337ff30cd2c218f0fffb
Alex <zc00gii@gmail.com>
2010-07-08 19:49:51 -0400

    got mouse stuff working

--- opengl-import-experiments.lisp
+++ opengl-import-experiments.lisp
@@ -3,19 +3,37 @@
 
 (in-package :how.opengl-import)
 
+(defvar *screen-height* 400)
+(defvar *screen-width* 500)
+
+(defun warp-mouse (x y)
+  (sdl:set-event-filter :mouse-motion-event
+			(lambda (event) nil))
+  (sdl-cffi::sdl-warp-mouse (multiple-value-bind
+				  (xx) (round
+					(float x))
+			      xx)
+			    (multiple-value-bind
+				  (yy) (round
+					(float y))
+			      yy))
+  (sdl:set-event-filter :mouse-motion-event
+			(lambda (event) t)))
+
 (defun 3d-test-1 ()
-  (let ((old-y 0) (old-x 0) (view-x 0) (view-y 0) (trans-z -60))
   (sdl:with-init ()
-    (sdl:window 500 400
+    (sdl:window *screen-width* *screen-height*
                 :title-caption "OpenGL Example"
                 :icon-caption "OpenGL Example"
                 :opengl t
                 :opengl-attributes '((:SDL-GL-DOUBLEBUFFER 1)))
     (sdl:enable-key-repeat nil nil)
-    (gl:viewport 0 0 500 400)
+    (sdl:show-cursor nil)
+    (warp-mouse (/ *screen-width* 2) (/ *screen-height* 2))
+    (gl:viewport 0 0 *screen-width* *screen-height*)
     (gl:matrix-mode :projection)
     (gl:load-identity)
-    (glu:perspective 45.0 (/ 500 400) 1.0 500.0)
+    (glu:perspective 45.0 (/ *screen-width* *screen-height*) 1.0 500.0)
     (gl:matrix-mode :modelview)
     (gl:load-identity)
     (gl:clear-color 0 0 0 0)
@@ -26,27 +44,21 @@
       (:key-repeat-on t)
       (:key-down-event (:key key)
                        (when (sdl:key= key :sdl-key-escape)
-			 (sdl:push-quit-event)))
-      (:mouse-motion-event (:x x :y y)
-			   ;(setf view-x (- x 230))
-			   ;(setf view-y (+ (- y) 160)))
-			   (when (> y old-y)
-			     (gl:translate 0 1 0)
-			     (setf old-y (+ old-y 1)))
-			   (when (< y old-y)
-			     (gl:translate 0 -1 0)
-			     (setf old-y (- old-y 1)))
-			   (when (> x old-x)
-			     (gl:translate 0 1 0)
-			     (setf old-x (+ old-x 1)))
-			   (when (< x old-x)
-			     (gl:translate 0 -1 0)
-			     (setf old-x (- old-x 1))))
-
+			 (sdl:push-quit-event))
+		       (when (sdl:key= key :sdl-key-r)
+			 (warp-mouse (/ *screen-width* 2)
+				     (/ *screen-height* 2))))
+      (:mouse-motion-event (:x-rel x-rel :y-rel y-rel :x x :y y)
+			;   (format t "x-rel: ~d y-rel: ~d x: ~d y: ~d~%"
+			;	   x-rel y-rel x y)
+			   (gl:viewport (- x 230) (+ (- y) 160)
+					*screen-width* *screen-height*))
+			;   (gl:translate (float (/ x-rel 20)) real buggy
+			;		 (float (/ y-rel 20)) 0))
       (:mouse-button-down-event (:button button)
-				(when (= button sdl:sdl-button-wheel-up)
-				  (gl:translate 0 0 1))
-				(when (= button sdl:sdl-button-wheel-down)
+			 	(when (= button sdl:sdl-button-wheel-up)
+			 	  (gl:translate 0 0 1))
+			 	(when (= button sdl:sdl-button-wheel-down)
 				  (gl:translate 0 0 -1)))
       (:idle ()
              (gl:clear :color-buffer-bit)
@@ -55,7 +67,7 @@
              (gl:enable-client-state :vertex-array)
              (draw-thing)
              (gl:flush)
-             (sdl:update-display))))))
+             (sdl:update-display)))))
 
 (defun normalize (vector)
   (let ((d (sqrt (apply #'+ (loop for i across vector
@@ -71,7 +83,7 @@
         #(5.0 5.0 5.0) #(5.0 -5.0 5.0)
         #(-5.0 -5.0 5.0) #(-5.0 5.0 5.0))
     (gl:bind-gl-vertex-array arr1)
-    (gl:rotate 1 1 1 3)
+    ;gl:rotate 1 1 1 3)
     (gl:polygon-mode :front-and-back :line)
     (with-gl-array-values (arr2 :unsigned-int)
         '(0 1 2 3 4 7 6 5 0 4 5 1 1 5 6 2 2 6 7 3 4 0 3 7)
