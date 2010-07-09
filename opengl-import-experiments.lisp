d442263aa318dc730908b07003eb0ab8b0f8ceef
Alex <zc00gii@gmail.com>
2010-07-08 20:38:29 -0400

    moved with-gl-array-values and such to cl-opengl

--- opengl-import-experiments.lisp
+++ opengl-import-experiments.lisp
@@ -92,7 +92,7 @@
 
 
 (defun draw-thing ()
-  (with-gl-array-values (arr1 'vertex '(x y z))
+  (gl:with-gl-array-values (arr1 'vertex '(x y z))
       '(#(5.0 5.0 -5.0) #(5.0 -5.0 -5.0)
         #(-5.0 -5.0 -5.0) #(-5.0 5.0 -5.0)
         #(5.0 5.0 5.0) #(5.0 -5.0 5.0)
@@ -100,53 +100,9 @@
     (gl:bind-gl-vertex-array arr1)
     ;gl:rotate 1 1 1 3)
     (gl:polygon-mode :front-and-back :line)
-    (with-gl-array-values (arr2 :unsigned-int)
+    (gl:with-gl-array-values (arr2 :unsigned-int)
         '(0 1 2 3 4 7 6 5 0 4 5 1 1 5 6 2 2 6 7 3 4 0 3 7)
       (gl:draw-elements :polygon arr2))))
-
-(gl:define-gl-array-format vertex
-  (gl:vertex :type :float :components (x y z)))
-
-(defmacro with-gl-array-values ((var type &optional components count) values
-                                &body body)
-  ;; Might want to make it so component types can nest:
-  ;; (x y z (w v x)) or whatever
-  (nutils:once-only (values)
-    `(gl:with-gl-array (,var ,type :count (or ,count (length ,values)))
-       (setf (get-arrays ,var ,components) ,values)
-       ;; Might want to have something that lets us bind stuff
-       ;;automatically in teh arglists, but not a top priority.
-       ;;(gl:bind-gl-vertex-array ,var)
-       ,@body)))
-
-(defun get-array (gl-vector &optional (index 0) components)
-  (if components
-      (apply #'vector
-             (mapcar (lambda (component)
-                       (gl:glaref gl-vector index component))
-                     components))
-      (gl:glaref gl-vector index)))
-
-(defun (setf get-array) (item gl-vector &optional (index 0) components)
-  (if components
-      (loop for element across item
-         for component in components
-         do (setf (gl:glaref gl-vector index component) element))
-      (setf (gl:glaref gl-vector index) item)))
-
-(defun get-arrays (gl-array &optional components (count (gl::gl-array-size gl-array)))
-  (loop for i from 0 to (1- count)
-       collect (get-array gl-array i components)))
-
-(defun (setf get-arrays) (vectors gl-array
-                          &optional components (count (gl::gl-array-size gl-array)))
-    (assert (<= (length vectors) count) () "More vectors then ~D
-Vectors are: ~A" count vectors)
-  (loop for vector in vectors
-     for i from 0
-     do (setf (get-array gl-array i components) vector)))
-
-
 
 #+ () (defparameter *source* (cxml:parse-file "/home/james/blender25/.x3d" (cxml-dom:make-dom-builder)))
 
