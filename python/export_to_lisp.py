c02e78f75ef374c920483f8630b0b08016bb8efe
Alex <zc00gii@gmail.com>
2010-07-14 14:39:20 -0400

    fixed indentation

--- python/export_to_lisp.py
+++ python/export_to_lisp.py
@@ -4,20 +4,20 @@ import bpy
 
 def export_to_lisp(filepath):
 	"' You must use this with blender '"
-    	out = file(filepath, 'w')
-	sce = bpy.data.scenes.active
-	ob = sce.objects.active
-	mesh = ob.getData(mesh=1)
-        out.write("((:vertices")
+    out = file(filepath, 'w')
+    sce = bpy.data.scenes.active
+    ob = sce.objects.active
+    mesh = ob.getData(mesh=1)
+    out.write("((:vertices ")
 	for vert in mesh.verts:
             out.write( '#(%f $%f %f)' % (vert.co.x,\
                                           vert.co.y, vert.co.z))
         out.write(")\n")
-	out.write('(:faces')
-	for face in mesh.faces:
-		out.write('(')
-		for vert in face.v:
-			out.write( '%i ' % (vert.index + 1))
-		out.write(')\n')
-	out.write("))")
-	out.close()
+    out.write('(:faces')
+    for face in mesh.faces:
+        out.write('(')
+        for vert in face.v:
+            out.write( '%i ' % (vert.index + 1))
+        out.write(')\n')
+    out.write("))")
+    out.close()
